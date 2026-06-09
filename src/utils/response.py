014a6631f21e871c1dc312e3453908d2ce532fb9
emin ölmez <e.olmez42@gmail.com>
2026-06-09 12:52:31 +0300

    Update response.py

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,25 +1,26 @@
 from sdks.novavision.src.helper.package import PackageHelper
-from components.DetectionMatcher.src.models.DetectionMatcherModel import (
-    DetectionMatcher, 
-    PackageConfigs, 
-    ConfigExecutor, 
-    DetectionMatcherOutputs, 
-    DetectionMatcherResponse, 
-    DetectionMatcherExecutor, 
+from components.DetectionMatcher.src.models.PackageModel import (
+    PackageModel,
+    PackageConfigs,
+    ConfigExecutor,
+    DetectionMatcherOutputs,
+    DetectionMatcherResponse,
+    DetectionMatcherExecutor,
     OutputDetections
 )
 
+
 def build_response(context):
     output_detections_obj = OutputDetections(value=context.outputDetections)
-    
+
     outputs = DetectionMatcherOutputs(outputDetections=output_detections_obj)
     packageResponse = DetectionMatcherResponse(outputs=outputs)
     packageExecutor = DetectionMatcherExecutor(value=packageResponse)
-    
+
     executor = ConfigExecutor(value=packageExecutor)
     packageConfigs = PackageConfigs(executor=executor)
-    
-    package = PackageHelper(packageModel=DetectionMatcher, packageConfigs=packageConfigs)
+
+    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
-    
+
     return packageModel
